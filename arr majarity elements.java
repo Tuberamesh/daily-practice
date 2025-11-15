8348bf6a21c6fc5c72b6885c7fe0dd2ba6c27175
the coder99 <r6904633@gmail.com>
2025-11-15 20:46:35 +0530

    n/3

--- arr majarity elements.java
+++ arr majarity elements.java
@@ -5,7 +5,7 @@ class Solution {
 
         // 1st approach is sorting
 
-        
+
         // Arrays.sort(nums);
         // return nums[nums.length / 2];
 
@@ -28,5 +28,38 @@ class Solution {
             
         }
         return test;
+    }
+}
+
+//problem no 229. Majority Element n/3
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+
+        Arrays.sort(nums);  
+        List<Integer> list = new ArrayList<>();
+
+        int n = nums.length;
+        int limit = n / 3;
+
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+            } else {
+                if (count > limit) {
+                    list.add(nums[i - 1]);
+                }
+                count = 1;
+            }
+        }
+
+        
+        if (count > limit) {
+            list.add(nums[n - 1]);
+        }
+
+        return list;
     }
 }
