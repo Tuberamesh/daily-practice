00ab26e61d5f6e61895d7c1a5f3723db227c610e
the coder99 <r6904633@gmail.com>
2025-11-15 21:09:05 +0530

    pow 2nd approach

--- recursion.java
+++ recursion.java
@@ -1,23 +1,51 @@
-//this is  leetcode  50. Pow(x, n) problem solution
-
 class Solution {
     public double myPow(double x, int n) {
         if (n == 0) return 1;
-        if (x==0) return 0;
+        if (x == 0) return 0;
 
-        long N = n;   // convert to long to handle Integer.MIN_VALUE
+        long N = n;
+        double result = 1; 
+
+        
+        boolean isNeg = false;
         if (N < 0) {
-            x = 1 / x;
-            N = -N;
+            isNeg = true;
+            N = N * -1;
         }
-        double result = 1;
+
+        
         while (N > 0) {
-            if ((N & 1) == 1)
-            result *= x;  // multiply if current power is odd
-            x *= x;                          // square x
-            N >>= 1;                         // divide N by 2
+            if (N % 2 == 1) {
+                result = result * x;
+                N--;
+            } else {
+                x = x * x;
+                N = N / 2;
+            }
         }
+
+        
+        if (isNeg) {
+            result = 1.0 / result;
+        }
+
         return result;
     }
 }
 
+        
+        
+          // convert to long to handle Integer.MIN_VALUE
+        // if (N < 0) {
+        //     x = 1 / x;
+        //     N = -N;
+        // }
+        // double result = 1;
+        // while (N > 0) {
+        //     if ((N & 1) == 1)
+        //     result *= x;  // multiply if current power is odd
+        //     x *= x;                          // square x
+        //     N >>= 1;                         // divide N by 2
+        // }
+        // return result;
+ 
