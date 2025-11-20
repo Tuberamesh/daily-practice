1dcbb240862db3fdd39a8e81b4c413fdcdba3cb6
the coder99 <r6904633@gmail.com>
2025-11-20 23:36:05 +0530

    coment the approach

--- hard qns.java
+++ hard qns.java
@@ -48,36 +48,36 @@ class Solution {
 //rainwater trapping problem 
 // 42 question leetcode
 
-class Solution {
-    public int trap(int[] height) {
-        int n=height.length;
-        int [] prefix=new int[n];
-        int [] suffix=new int[n];
+// class Solution {
+//     public int trap(int[] height) {
+//         int n=height.length;
+//         int [] prefix=new int[n];
+//         int [] suffix=new int[n];
         
-        int total =0;
+//         int total =0;
 
-        prefix[0]=height[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=Math.max(prefix[i-1],height[i]);
-        }
-
-
-        suffix[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=Math.max(suffix[i+1],height[i]);
-        }
+//         prefix[0]=height[0];
+//         for(int i=1;i<n;i++){
+//             prefix[i]=Math.max(prefix[i-1],height[i]);
+//         }
 
 
-        for(int i=0;i<n;i++){
-            int leftmax=prefix[i]; 
-            int rightmax=suffix[i];
+//         suffix[n-1]=height[n-1];
+//         for(int i=n-2;i>=0;i--){
+//             suffix[i]=Math.max(suffix[i+1],height[i]);
+//         }
 
-            if(height[i]<leftmax && height[i]<rightmax){
-                total+=Math.min(leftmax,rightmax)-height[i];
-            }
-        }
 
-        return total;
-    }
-}
+//         for(int i=0;i<n;i++){
+//             int leftmax=prefix[i]; 
+//             int rightmax=suffix[i];
+
+//             if(height[i]<leftmax && height[i]<rightmax){
+//                 total+=Math.min(leftmax,rightmax)-height[i];
+//             }
+//         }
+
+//         return total;
+//     }
+// }
 
