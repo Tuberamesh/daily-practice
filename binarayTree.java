1b32dd29a2b9253a9e84688f5bc330efd5b91e91
the coder99 <r6904633@gmail.com>
2025-11-26 22:50:20 +0530

    pre order

--- binarayTree.java
+++ binarayTree.java
@@ -30,3 +30,42 @@ class Solution {
         post.add(root.val);
 }
 }
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> ans=new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        
+        preOrder(root);
+        return ans;
+    }
+    private void preOrder(TreeNode root){
+            if(root==null){
+                return;
+            }
+            ans.add(root.val);
+        preOrder(root.left);
+         preOrder(root.right);
+        
+            
+        }
+
+       // preorderTraversal(root.val);
+        
+    }
