7e171d5a89d5788ec8e4c4885dd98f0f24acf911
the coder99 <r6904633@gmail.com>
2025-11-28 23:53:19 +0530

    min depth

--- binarayTree.java
+++ binarayTree.java
@@ -226,3 +226,52 @@ class Solution {
         return avgList;
     }
 }
+
+
+
+//mimimum depth of binary tree
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minDepth(TreeNode root) {
+
+        if (root == null) return 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int level = 1;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+
+                // First time we see a leaf — that’s min depth
+                if (node.left == null && node.right == null) {
+                    return level;
+                }
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            level++;
+        }
+
+        return level;
+    }
+}
