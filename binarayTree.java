8b9edf22926bb826e39f0a6108d1651bcc2fb50f
the coder99 <r6904633@gmail.com>
2025-11-28 23:55:48 +0530

    max depth

--- binarayTree.java
+++ binarayTree.java
@@ -275,3 +275,56 @@ class Solution {
         return level;
     }
 }
+
+
+//maximum depth of binary tree
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ 
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+
+         if (root == null) return 0;
+
+
+        Queue<TreeNode> queueElements=new LinkedList<>();
+        int countLevels=0;
+        queueElements.add(root);
+         
+        while(!queueElements.isEmpty()){
+            int countAtNodes=queueElements.size();
+            // if(countAtNodes==0)
+            // return countLevels;
+        
+        while(countAtNodes>0){
+            TreeNode treeElements=queueElements.poll();
+
+            if (treeElements.left != null)
+                   queueElements.add(treeElements.left);
+            if (treeElements.right != null)
+                   queueElements.add(treeElements.right);
+
+
+             countAtNodes--;
+        }
+        countLevels++;
+        
+    }
+    return countLevels;
+}
+}
