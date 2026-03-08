b599bc36ee81ef110a9b611024ad2705f223d22c
Tuberamesh <r6904633@gmail.com>
2026-03-08 15:54:57 +0530

    product of arr

--- arra(287,74).java
+++ arra(287,74).java
@@ -119,3 +119,28 @@ class Solution {
         
     }
 }
+// question 238 product of array except self..
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n=nums.length;
+        int left[]=new int[n];
+        int right[]=new int[n];
+          int ans[]=new int[n];
+        
+
+        left[0]=1;
+        for(int i=1;i<n;i++){
+            left[i]=left[i-1]*nums[i-1];
+        }
+        right[n-1]=1;
+         for(int i=right.length-2;i>-1;i--){
+            right[i]=right[i+1]*nums[i+1];
+        }
+
+         for(int i=0;i<n;i++){
+            ans[i]=left[i]*right[i];
+        }
+        return ans;
+        
+    }
+}
