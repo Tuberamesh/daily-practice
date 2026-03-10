d24c888ed23081d9acf635c3a29c756fc03be8c2
Tuberamesh <r6904633@gmail.com>
2026-03-10 22:43:15 +0530

    2 sum sorted

--- arra(287,74).java
+++ arra(287,74).java
@@ -227,3 +227,28 @@ class Solution {
         
     }
 }
+
+// question 167 two sum 2 input array is sorted..
+//time complexity is O(n^2)
+// class Solution {
+//     public int[] twoSum(int[] numbers, int target) {
+
+//         if(numbers.length==0){
+//             return;
+//         }
+
+//         int n=numbers.length;
+//         int res[]=new int[2];
+
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(numbers[i]+numbers[j]==target){
+//                     res[0]=i+1;
+//                     res[1]=j+1;
+//                 }
+//             }
+//         }
+
+//         return res;
+//     }
+// }
