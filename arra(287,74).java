28b7502e55f31cc242919bfa3a23c31e45c81b19
Tuberamesh <r6904633@gmail.com>
2026-03-10 22:44:04 +0530

    2 sum sorted better sol

--- arra(287,74).java
+++ arra(287,74).java
@@ -252,3 +252,27 @@ class Solution {
 //         return res;
 //     }
 // }
+
+// time complexity is O(n) and space complexity is O(1)
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0;
+        int r = numbers.length - 1;
+
+        while (l < r) {
+            int currentSum = numbers[l] + numbers[r];
+
+            if (currentSum == target) {
+                return new int[] {l + 1, r + 1};
+            } 
+            else if (currentSum > target) {               
+                r--;
+            } 
+            else {               
+                l++;
+            }
+        }
+
+        return new int[0];
+    }
+}
