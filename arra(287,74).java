c037b7b70bb4a9202538850b219d3b6220285073
Tuberamesh <r6904633@gmail.com>
2026-03-10 23:25:50 +0530

    3 sum

--- arra(287,74).java
+++ arra(287,74).java
@@ -276,3 +276,45 @@ class Solution {
         return new int[0];
     }
 }
+
+// question 15 three sum..
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        int n=nums.length;
+        if(nums==null || nums.length<3)
+        return new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        Set<List<Integer>> res=new HashSet<>();
+
+        for(int i=0;i<n-1;i++){
+
+        int left=i+1;
+        int right=n-1;
+
+        while(left<right){
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if(sum==0){
+                res.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                left++;
+                right--;
+            }
+
+            else if(sum<0){
+                left++;
+            }
+            else{
+                right--;
+            }
+          
+        }
+        }
+
+        return new ArrayList<>(res);
+        
+    }
+}
