7a8000ddb573facb6726348bdcd7b9c83718bb3b
Tuberamesh <r6904633@gmail.com>
2026-03-11 22:20:04 +0530

    rain water hard

--- arra(287,74).java
+++ arra(287,74).java
@@ -344,3 +344,40 @@ class Solution {
         return max;
     }
 }
+
+
+// question 42 trapping rain water..
+class Solution {
+    public int trap(int[] height) {
+        int n=height.length;
+        int left=0;
+        int right =n-1;
+        int leftmax=0,rightmax=0;
+        int total=0;
+
+        while(left<right){
+            if(height[left]<=height[right]){
+                if(height[left]>=leftmax){
+                    leftmax=height[left];
+                }
+                else{
+                    total+=leftmax-height[left];
+                
+                }left++;
+            }
+            else{
+                  if(height[right]>=rightmax){
+                    rightmax=height[right];
+                }
+                else{
+                    total+=rightmax-height[right];
+                    
+                }right--;
+            }
+            }
+             return total;
+        }
+
+       
+        
+    }
