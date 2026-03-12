1f26fdb5a820ad95fca7be904cbc370def96ed86
Tuberamesh <r6904633@gmail.com>
2026-03-12 22:43:22 +0530

    qn 20

--- arra(287,74).java
+++ arra(287,74).java
@@ -381,3 +381,26 @@ class Solution {
        
         
     }
+
+    // question 20 valid parentheses..
+
+    class Solution {
+    public boolean isValid(String s) {
+
+      Stack<Character> stack=new Stack();  
+      for(char c:s.toCharArray()){
+        if(c=='(') stack.push(')');
+        else if (c=='{') stack.push('}');
+        else if (c=='[') stack.push(']');
+
+        else if (stack.empty() || stack.pop()!=c)
+        return false;
+
+      }
+      return stack.empty();
+
+     
+    
+        
+    }
+}
