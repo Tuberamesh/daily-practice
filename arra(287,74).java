6490f44a3c9def6d971abd81e9887fa62222a758
Tuberamesh <r6904633@gmail.com>
2026-03-12 23:29:09 +0530

    evaluate

--- arra(287,74).java
+++ arra(287,74).java
@@ -450,3 +450,39 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+
+// question 150 evaluate reverse polish notation..
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        //Stack<Character> stack=new Stack();
+
+        for(String c:tokens){
+            if(c.equals("+")){
+                stack.push(stack.pop() +stack.pop());
+            }
+            else if(c.equals("-")){
+                int a=stack.pop();
+                int b=stack.pop();
+
+                stack.push(b-a);
+            }
+            else if(c.equals("*")){
+                stack.push(stack.pop()*stack.pop());
+            }
+            else if(c.equals("/")){
+                int a=stack.pop();
+                int b=stack.pop();
+
+                stack.push(b/a);
+            }
+            else{
+                stack.push(Integer.parseInt(c));
+            }
+        }
+        return stack.pop();
+        
+    }
+}
