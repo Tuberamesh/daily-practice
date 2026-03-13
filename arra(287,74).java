49cb97f89dc38f9e6334ffb6750a113041b76073
Tuberamesh <r6904633@gmail.com>
2026-03-13 22:04:26 +0530

    daily tempt

--- arra(287,74).java
+++ arra(287,74).java
@@ -486,3 +486,27 @@ class Solution {
         
     }
 }
+
+// question 739 daily temperatures..
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        Stack<Integer> stack = new Stack<>();
+        int res[] = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+               while (!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                res[i] = 0; 
+            } else {
+                res[i] = stack.peek() - i; 
+            }
+
+            stack.push(i);
+        }
+        
+        return res;
+    }
+}
