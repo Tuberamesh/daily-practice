b465eaffc884761fb2e8553ab236fb66a28fbc85
Tuberamesh <r6904633@gmail.com>
2026-03-16 23:27:05 +0530

    longest repeating char

--- arra(287,74).java
+++ arra(287,74).java
@@ -676,3 +676,36 @@ class Solution {
         return longest.length();
     }
 }
+
+// question 424 longest repeating character replacement..
+public class Solution {
+    public int characterReplacement(String s, int k) {
+        int res=0;
+        Set<Character> charset =new HashSet<>();
+
+        for(char c:s.toCharArray()){
+            charset.add(c);
+        }
+
+        for(char c:charset){
+            int count=0;
+            int left=0;
+            for(int right=0;right<s.length();right++){
+                if(s.charAt(right)==c){
+                    count++;
+                }
+            
+            while((right-left+1) - count>k){
+                if(s.charAt(left)==c){
+                    count--;
+                }
+                left++;
+            }
+
+            res=Math.max(res,right-left+1);
+        }
+        
+    }
+    return res;
+}
+}
