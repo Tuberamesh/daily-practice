1d88114c98b3395b5b2294fce7f50354a05c90b0
Tuberamesh <r6904633@gmail.com>
2026-03-19 23:01:52 +0530

    reorder

--- arra(287,74).java
+++ arra(287,74).java
@@ -773,3 +773,40 @@ public class Solution {
         return false;
     }
 }
+
+// question 143 reorder list..
+class Solution {
+    public void reorderList(ListNode head) {
+         if (head == null || head.next == null) 
+      return;
+
+      ListNode p1=head;
+      ListNode p2=head;
+
+      while(p2.next!=null && p2.next.next!=null){
+        p1=p1.next;
+        p2=p2.next.next;
+      }
+        ListNode preMid=p1;
+        ListNode preCurr=preMid.next;
+
+        while(preCurr.next!=null){
+            ListNode curr=preCurr.next;   
+            preCurr.next=curr.next;
+            curr.next=preMid.next;
+            preMid.next=curr;
+        }
+
+        p1=head;
+        p2=preMid.next;
+
+        while(p1!=preMid){
+            preMid.next=p2.next;
+            p2.next=p1.next;
+            p1.next=p2;
+            p1=p2.next;
+            p2=preMid.next;
+        }
+      
+    }
+}
