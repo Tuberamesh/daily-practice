26e9d92072f8e3ee25d116abbdd0825ce76f7039
Tuberamesh <r6904633@gmail.com>
2026-03-19 22:00:21 +0530

    cycle linked list

--- arra(287,74).java
+++ arra(287,74).java
@@ -753,3 +753,23 @@ class Solution {
         return dummy.next;
     }
 }
+
+// question 141 linked list cycle..
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slowptr=head;
+        ListNode fastptr=head;
+
+        while(slowptr!=null && fastptr!=null && fastptr.next!=null ){
+            slowptr=slowptr.next;
+            fastptr=fastptr.next.next;
+
+            if(slowptr==fastptr){
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
