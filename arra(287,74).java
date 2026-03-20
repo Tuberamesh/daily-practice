3cb36b24d18a5b70d72af493d61e8e0987f57ede
Tuberamesh <r6904633@gmail.com>
2026-03-20 19:54:37 +0530

    LRU CASHE

--- arra(287,74).java
+++ arra(287,74).java
@@ -923,3 +923,27 @@ class Solution {
     return -1;
   }
     }
+
+    // quesstion lru cache..
+    class LRUCache {
+    private final Map<Integer, Integer> cache;
+    private final int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new LinkedHashMap<>(capacity, 0.75f, true) {
+        protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+            return size() > LRUCache.this.capacity;
+            }
+        };
+        
+    }
+    
+   public int get(int key) {
+        return cache.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        cache.put(key, value);
+    }
+}
