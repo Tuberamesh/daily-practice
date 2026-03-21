9d440cfe7cc5020e01f49f5dd615c429ed12abde
Tuberamesh <r6904633@gmail.com>
2026-03-21 21:49:23 +0530

    matrix

--- arra(287,74).java
+++ arra(287,74).java
@@ -962,3 +962,11 @@ class Solution {
         return false;
     }
 }
+
+// time complexity is O(log(m*n)) and space complexity is O(1)
+class Solution {
+    public int search(int[] nums, int target) {
+ int index = Arrays.binarySearch(nums, target);
+        return index >= 0 ? index : -1;
+    }
+}
