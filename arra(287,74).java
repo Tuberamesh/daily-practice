15fed10283ed90d961abc1d8529f5924f59fa396
Tuberamesh <r6904633@gmail.com>
2026-03-21 22:19:08 +0530

    koko banana

--- arra(287,74).java
+++ arra(287,74).java
@@ -970,3 +970,38 @@ class Solution {
         return index >= 0 ? index : -1;
     }
 }
+
+
+//koko eating bananas question 875..
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = 0;
+        for (int pile : piles) {
+            right = Math.max(right, pile);
+        }
+
+        int res = right;
+
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            long totalTime = 0;
+            for (int pile : piles) {
+            
+                totalTime += (long) Math.ceil((double) pile / mid);
+            }
+
+            if (totalTime <= h) {
+                res = mid;
+                right = mid - 1;
+            } else {
+            
+                left = mid + 1;
+            }
+        }
+
+        return res;
+    }
+}
